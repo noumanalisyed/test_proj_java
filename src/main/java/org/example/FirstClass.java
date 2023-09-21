49ea0de1da6b9fbadf384035052346cc8e744635
noumanalisyed <nouman.ali.syed@gmail.com>
2023-09-21 11:59:22 +0500

    Added FirstClass and print function

---
+++ src/main/java/org/example/FirstClass.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class FirstClass {
+    private String message;
+    public FirstClass(){
+        message = "Welcome to my First Class !!";
+    }
+    public void print(){
+        System.out.println("message = "+message);
+    }
+}
---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        FirstClass firstClass = new FirstClass();
+        firstClass.print();
+        System.out.println("Hello world!");
+    }
+}
